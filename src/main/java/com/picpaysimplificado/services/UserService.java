9ca37a7fb3ccc68bb2f98806e20cb0d7eb8e1cde
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-06-23 17:58:01 -0300

    fix: unused import removed

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.picpaysimplificado.domain.user.User;
