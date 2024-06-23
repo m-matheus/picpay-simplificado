83715fc5234e8de8153dfe37ac6b33b12f164d9c
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-06-23 17:56:23 -0300

    feat: new user methods

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -1,12 +1,15 @@
 package com.picpaysimplificado.services;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 
 @Service
@@ -26,8 +29,18 @@ public class UserService {
     public User findUserById(Long id) {
         return repository.findById(id).orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
+    
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
 
     public User saveUser(User user) {
         return repository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 }
