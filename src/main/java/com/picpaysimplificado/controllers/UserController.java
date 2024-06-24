235a1ec83df651ba38e035886ee3ac39d56bed1c
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-06-24 14:18:44 -0300

    fix: createUser requestBody

--- src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -4,10 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.UserDTO;
@@ -21,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(UserDTO user) {
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
